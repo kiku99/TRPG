948274028678a3c28e510daa99ce4dccc083908f
kiku99 <elsd0326@gmail.com>
2022-06-24 16:00:43 +0900

    종족별 스탯 설정

--- src/Status.java
+++ src/Status.java
@@ -21,10 +21,35 @@ public class Status {
     // 종족 스탯 설정
     public void setRaceStatus(){
         if (characterMap.get("race").equals("OAK")){
-            statusMap.put(status.STR.name(), 5);
-            System.out.println(statusMap.get(status.STR.name()));
+            statusMap.put(status.STR.name(), 4);
+            statusMap.put(status.DEX.name(), 1);
+            statusMap.put(status.INT.name(), 0);
+            statusMap.put(status.CON.name(), 4);
+            statusMap.put(status.BLS.name(), 1);
+        }
+        else if (characterMap.get("race").equals("HUMAN")){
+            statusMap.put(status.STR.name(), 1);
+            statusMap.put(status.DEX.name(), 3);
+            statusMap.put(status.INT.name(), 3);
+            statusMap.put(status.CON.name(), 1);
+            statusMap.put(status.BLS.name(), 2);
+        }
+        else if (characterMap.get("race").equals("ELF")){
+            statusMap.put(status.STR.name(), 1);
+            statusMap.put(status.DEX.name(), 2);
+            statusMap.put(status.INT.name(), 3);
+            statusMap.put(status.CON.name(), 0);
+            statusMap.put(status.BLS.name(), 4);
+        }
+        else if (characterMap.get("race").equals("DWARF")){
+            statusMap.put(status.STR.name(), 2);
+            statusMap.put(status.DEX.name(), 4);
+            statusMap.put(status.INT.name(), 1);
+            statusMap.put(status.CON.name(), 2);
+            statusMap.put(status.BLS.name(), 0);
         }
     }
+
 
     // 직업 스탯 설정
     public void setJobsStatus(){
