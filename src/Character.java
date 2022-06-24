991f133596e97249bcaf8c72adf67bceca53f5a9
kiku99 <elsd0326@gmail.com>
2022-06-24 14:08:23 +0900

    Interactive.java 추가 및 코드 재배치

--- src/Character.java
+++ src/Character.java
@@ -3,79 +3,17 @@ import java.util.Scanner;
 
 public class Character {
 
-    public static Character character;
-    Status status = new Status();
-    HashMap<String, String> characterMap = new HashMap<String, String>();
-
-    Scanner scanner = new Scanner(System.in);
-
+    static HashMap<String, String> characterMap = new HashMap<String, String>();
     public enum Jobs{
         KNIGHT, THIEF, MONK, ARCHER
         }
-
     public enum Race{
         OAK, HUMAN, ELF, DWARF
     }
 
-public void createCharacter(){
-    System.out.println("캐릭터를 생성합니다.");
-    selectRace();
-    selectJobs();
-    setStatus();
-}
-
-public void selectJobs(){
-    System.out.println("직업을 선택해 주세요");
-    System.out.println("1.기사 2.도적 3.수도승 4.궁수");
-    int jobs = scanner.nextInt();
-    switch (jobs) {
-        case 1 -> {
-            characterMap.put("jobs", Jobs.KNIGHT.name());
-            System.out.println("당신의 직업은 기사입니다.\n");
-        }
-        case 2 -> {
-            characterMap.put("jobs", Jobs.THIEF.name());
-            System.out.println("당신의 직업은 도적입니다.\n");
-        }
-        case 3 -> {
-            characterMap.put("jobs", Jobs.MONK.name());
-            System.out.println("당신의 직업은 수도승입니다.\n");
-        }
-        case 4 -> {
-            characterMap.put("jobs", Jobs.ARCHER.name());
-            System.out.println("당신의 직업은 궁수입니다.\n");
-        }
-    }
-}
-
-public void selectRace(){
-    System.out.println("종족을 선택해 주세요");
-    System.out.println("1.오크 2.인간 3.엘프 4.드워프");
-    int race = scanner.nextInt();
-    switch (race) {
-        case 1 -> {
-            characterMap.put("race", Race.OAK.name());
-            System.out.println("당신의 종족은 오크입니다.\n");
-        }
-        case 2 -> {
-            characterMap.put("race", Race.HUMAN.name());
-            System.out.println("당신의 종족은 인간 입니다.\n");
-        }
-        case 3 -> {
-            characterMap.put("race", Race.ELF.name());
-            System.out.println("당신의 종족은 엘프입니다.\n");
-        }
-        case 4 -> {
-            characterMap.put("race", Race.DWARF.name());
-            System.out.println("당신의 종족은 드워프입니다.\n");
-        }
-    }
-}
-
-    public HashMap<String, String> getCharacterMap() {
-        return characterMap;
-    }
-
+//public void selectJob(Character.Jobs job) {
+//        characterMap.put("jobs", job.name());
+//}
     public void setStatus(){
         if (characterMap.get("race").equals("OAK")){
             System.out.println("test oak");
--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Game {
-Character character = new Character();
-Menu menu = new Menu();
+Character character;
+Menu menu;
+//Interactive interactive;
+Scanner scanner;
 
-Scanner scanner = new Scanner(System.in);
+    public Game(){
+        this.menu = new Menu();
+        this.character = new Character();
+//        this.interactive = new Interactive();
+        this.scanner = new Scanner(System.in);
+    }
+
     public void start(){
         menu.mainMenu(); // 메인 메뉴 실행
-        int menuNum = scanner.nextInt();
-        switch (menuNum) {
-            case 1 -> character.createCharacter();
-            case 2 -> System.exit(0);
-        }
-
     }
 }
