8551b4f3439dc817c9ced970e5a32ba813bbf5c3
kiku99 <elsd0326@gmail.com>
2022-06-24 15:31:24 +0900

    Status.java 에서 종족, 직업 스탯 설정

--- src/Game.java
+++ src/Game.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Game {
 Character character;
 Menu menu;
+Status status;
 //Interactive interactive;
-Scanner scanner;
 
     public Game(){
         this.menu = new Menu();
         this.character = new Character();
+        this.status = new Status();
 //        this.interactive = new Interactive();
-        this.scanner = new Scanner(System.in);
     }
 
     public void start(){
         menu.mainMenu(); // 메인 메뉴 실행
+        status.setStatus();
     }
 }
--- src/Status.java
+++ src/Status.java
@@ -1,8 +1,37 @@
 import java.util.HashMap;
 
 public class Status {
+
+    Character character;
+    HashMap<String, String> characterMap = Character.characterMap;
+    HashMap<String, Integer> statusMap = new HashMap<String, Integer>();
+    public Status(){
+        character = new Character();
+
+    }
     public enum status{
-        STR, DEX, INT, CON, BLS
+        STR, DEX, INT, CON, BLS;
+    }
+
+    public void setStatus(){
+        setRaceStatus();
+        setJobsStatus();
+    }
+
+    // 종족 스탯 설정
+    public void setRaceStatus(){
+        if (characterMap.get("race").equals("OAK")){
+            statusMap.put(status.STR.name(), 5);
+            System.out.println(statusMap.get(status.STR.name()));
+        }
+    }
+
+    // 직업 스탯 설정
+    public void setJobsStatus(){
+        if (characterMap.get("jobs").equals("KNIGHT")){
+            statusMap.put(status.STR.name(), statusMap.get(status.STR.name()) + 5);
+            System.out.println(statusMap.get(status.STR.name()));
+        }
     }
 
     }
